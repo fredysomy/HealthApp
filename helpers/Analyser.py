47b06c093f5b256a4697962c3b16646405eeb824
fredysomy <fredysomy@gmail.com>
2020-11-03 07:21:38 -1000

    added analyser.py

--- helpers/Analyser.py
+++ helpers/Analyser.py
@@ -12,6 +12,10 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import requests
 import json
 from PyQt5.QtWidgets import QMessageBox
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from time import sleep
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
@@ -77,6 +81,7 @@ class Ui_Dialog(object):
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
         self.CitySearch.clicked.connect(self.getresource)
+        self.Visualiser.clicked.connect(self.analyserXY)
     def getresource(self):
         try:
             a=requests.get('https://api.covid19india.org/resources/resources.json')
@@ -110,7 +115,46 @@ class Ui_Dialog(object):
             msg.setWindowTitle('Error')
             msg.exec_()
         
-            
+    
+    def analyserXY(self):
+        data = pd.read_csv('teddy.csv')
+
+        Y = data.iloc[61:,1].values
+        R = data.iloc[61:,3].values
+        D = data.iloc[61:,5].values
+        X = data.iloc[61:,0]
+
+
+        axes = plt.axes()
+        axes.grid(linewidth = 0.4, color = "#28a9ff")
+        axes.set_facecolor("white")
+        axes.set_xlabel('\n',size=20,color='#4bb4f2')
+        axes.set_ylabel("total cases\n",size=20,color="#4bb4f2")
+
+        plt.xticks(rotation='vertical',size=20,color='black')
+        plt.yticks(size=20, color='black')
+        plt.tick_params(size=20,color='black')
+
+        for i,j in zip(X,Y):
+            axes.annotate(str(j),xy=(i,j+100),color='black',size='13')
+
+        axes.annotate('second lockdown', xy =(15.2, 860), 
+                     xytext =(20, 500),
+                     size='25', 
+                     arrowprops = dict(color ='black', 
+                                       shrink = 0.05))
+        plt.title('COVID-19 DAILY CONFIRMED',size=50,color='#28a9ff')
+
+
+        axes.plot(X,Y,
+            color="#00FFFF",
+            marker="o",
+            linewidth=4,
+            markersize=15,
+            markeredgecolor="#035E9B")
+
+        plt.show()
+
         
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
